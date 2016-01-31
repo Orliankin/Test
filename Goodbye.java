bd70d1affe80553aa591582efac12901dc35a892
Orliankin <mra8@mail.ru>
2016-01-31 09:03:45 +0200

    Goodbye.java

--- Goodbye.java
+++ Goodbye.java
@@ -1,5 +1,6 @@
-public class HelloWorld {
+public class Goodbye {
 	public static void main(String[] args) {
-		System.out.println("Goodbye");
+		System.out.print("Goodbye");
+		System.out.println(" to all!");
 	}
 }
